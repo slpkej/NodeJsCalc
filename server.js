e75403ca037fb3624eee8cee81ad2302b14749b1
slpkej <jkotani124@aol.com>
2018-07-15 03:07:51 -0400

    Second Commit

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+//Create variable and require readline sync from NPM
 var rs = require('readline-sync');
 
+//Create three variables asking for the numbers and operator
 var firstNumber = rs.question("Please enter your first number: ");
 var secondNumber = rs.question("Please enter your second number: ");
 var operator = rs.question("Please enter your operator: ");
 
+//Variable meshing the user numbers with operator
 var operation = firstNumber + operator + secondNumber;
 
+//variable for the result of var operation
 var result = eval(operation);
 
+//console.log the the result
 console.log(result);
